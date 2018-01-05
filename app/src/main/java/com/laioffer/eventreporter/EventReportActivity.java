6cd339131fb2ac3695d1159aa1394a5aa6ed0e56
szcbossman <songzicheng3@gmail.com>
2018-01-04 19:07:20 -0800

    location manager/firebase storage for pic submit

--- app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
@@ -1,5 +1,9 @@
 package com.laioffer.eventreporter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +23,12 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventReportActivity extends AppCompatActivity {
     private static final String TAG = EventReportActivity.class.getSimpleName();
@@ -29,6 +41,17 @@ public class EventReportActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private LocationTracker mLocationTracker;
+    private Activity mActivity;
+
+    private static int RESULT_LOAD_IMAGE = 1;
+    private ImageView img_event_picture;
+    private Uri mImgUri;
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
 
 
 
@@ -43,13 +66,33 @@ public class EventReportActivity extends AppCompatActivity {
         mImageViewCamera = (ImageView) findViewById(R.id.img_event_camera);
         mImageViewSend = (ImageView) findViewById(R.id.img_event_report);
         database = FirebaseDatabase.getInstance().getReference();
+        img_event_picture = (ImageView) findViewById(R.id.img_event_picture_capture);
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+
 
         mImageViewSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String key = uploadEvent();
+                if (mImgUri != null) {
+                    uploadImage(key);
+                    mImgUri = null;
+                }
             }
         });
+
+        mImageViewCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+
 
 
         //auth
@@ -76,6 +119,33 @@ public class EventReportActivity extends AppCompatActivity {
             }
         });
 
+
+        // check if GPS enabled
+        mActivity = this;
+        mLocationTracker = new LocationTracker(mActivity);
+        mLocationTracker.getLocation();
+        final double latitude = mLocationTracker.getLatitude();
+        final double longitude = mLocationTracker.getLongitude();
+
+        new AsyncTask<Void, Void, Void>() {
+            private List<String> mAddressList = new ArrayList<>();
+
+            @Override
+            protected Void doInBackground(Void... urls) {
+                mAddressList = mLocationTracker.getCurrentLocationViaJSON(latitude,longitude);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void input) {
+                if (mAddressList.size() >= 3) {
+                    mEditTextLocation.setText(mAddressList.get(0) + ", " + mAddressList.get(1) +
+                            ", " + mAddressList.get(2) + ", " + mAddressList.get(3));
+                }
+            }
+        }.execute();
+
+
     }
 
     @Override
@@ -89,6 +159,21 @@ public class EventReportActivity extends AppCompatActivity {
         super.onStop();
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+                Uri selectedImage = data.getData();
+                img_event_picture.setVisibility(View.VISIBLE);
+                img_event_picture.setImageURI(selectedImage);
+                mImgUri = selectedImage;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -128,5 +213,36 @@ public class EventReportActivity extends AppCompatActivity {
         });
         return key;
     }
+
+    /**
+     * Upload image and get file path
+     */
+    private void uploadImage(final String eventId) {
+        if (mImgUri == null) {
+            return;
+        }
+        StorageReference imgRef = storageRef.child("images/" + mImgUri.getLastPathSegment() + "_"
+                + System.currentTimeMillis());
+
+        UploadTask uploadTask = imgRef.putFile(mImgUri);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                @SuppressWarnings("VisibleForTests")
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.i(TAG, "upload successfully" + eventId);
+                database.child("events").child(eventId).child("imgUri").
+                        setValue(downloadUrl.toString());
+            }
+        });
+    }
+
 
 }
