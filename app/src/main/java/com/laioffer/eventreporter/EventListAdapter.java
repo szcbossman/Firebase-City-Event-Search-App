601fc77c0cfc3c0291923ebc148a14641dca7323
szcbossman <songzicheng3@gmail.com>
2018-01-06 16:44:59 -0800

    comment function implemented

--- app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
+++ app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
@@ -1,6 +1,7 @@
 package com.laioffer.eventreporter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
@@ -184,6 +185,8 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         holder.time.setText(Utils.timeTransformer(event.getTime()));
         holder.good_number.setText(String.valueOf(event.getLike()));
 
+        holder.comment_number.setText(String.valueOf(event.getCommentNumber()));
+
         if (event.getImgUri() != null) {
             final String url = event.getImgUri();
             holder.imgview.setVisibility(View.VISIBLE);
@@ -226,6 +229,17 @@ public class EventListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
                 });
             }
         });
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CommentActivity.class);
+                String eventId = event.getId();
+                intent.putExtra("EventID", eventId);
+                context.startActivity(intent);
+            }
+        });
+
 
     }
 
